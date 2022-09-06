3a33d96948d28a11eb62e41dbcb20138da4a2225
Mateus Garcia <mateus.garcia@motrix.global>
2022-09-06 13:42:53 -0300

    feat: load new files

--- src/scenes/loading/index.ts
+++ src/scenes/loading/index.ts
@@ -9,17 +9,25 @@ export class LoadingScene extends Scene {
     this.load.baseURL = "assets/"
 
     this.load.image("ball", "sprites/ball.png")
+    this.load.image("crosshair", "sprites/crosshair012.png")
+
     this.load.image("bullet", "sprites/bullet6.png")
 
     this.load.image("king", "sprites/king.png")
 
     this.load.atlas("a-king", "spritesheets/a-king.png", "spritesheets/a-king_atlas.json")
 
+    this.load.atlas("walk", "spritesheets/walk.png", "spritesheets/walk.json")
+
+    this.load.image("tiles", "tilemaps/iso/iso-64x64-outside.png")
+    this.load.image("tiles2", "tilemaps/iso/iso-64x64-building.png")
+    this.load.tilemapTiledJSON("map", "tilemaps/iso/isorpg.json")
+
     this.load.image({
       key: "tiles",
       url: "tilemaps/tiles/dungeon-16-16.png",
     })
-    this.load.tilemapTiledJSON("dungeon", "tilemaps/json/dungeon.json")
+    this.load.tilemapTiledJSON("dungeon", "tilemaps/json/new.json")
 
     this.load.spritesheet("tiles_spr", "tilemaps/tiles/dungeon-16-16.png", {
       frameWidth: 16,
